b16e1fb9eee2d6af45111d1ae7c854420eb77a71
Risoxas <davidag93@gmail.com>
2021-04-16 18:26:04 -0500

    included date and time

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@
 //         coverUrl:"https://picsum.photos/id/237/768/384",  /* portada del post*/
 //     }
 // } 
-
 // let replies = {
 //     reply1:{ /*entrada de comentario*/
 //         userId:1, /*id del usuario que comenta*/
@@ -32,28 +31,94 @@
 //         avatar:"https://media-exp1.licdn.com/dms/image/C5603AQFxZihNUVo-ng/profile-displayphoto-shrink_200_200/0/1517501855544?e=1623888000&v=beta&t=SaHOe6Q1nQkH-ZQYGZy8P1OimoTNq-ZAIwZFE0uleO8"
 //     }
 // }
+let userObject = {
+    key: '',
+    id: '',
+    name: '',
+    title: '',
+    text: '',
+    url: '',
+    date: '',
+    time: '',
+};
+let key = '';
+let upload = function() {
+    $.ajax({
+        method: "POST",
+        url: "https://taco-database-default-rtdb.firebaseio.com/user/users/crud.json",
+        data: JSON.stringify({userObject}),
+        success : response => {
+            console.log(response)
+        },
+        error : error => {
+            console.log(error)
+        },
 
-let cardHTML = `
-<div class="container">
-<div class="row">
-    <div class="col-12">
-        <div class="card mb-3">
-            ${postBodyHTML}
-            ${replyHTML}
-            <div class="reply-form">
-            <form action="">
-                <div class="form-group d-flex">
-                    <input type="text" class="form-control">
-                    <button class="btn btn-success">></button>
-                </div>
-            </form>
-        </div>
-        </div>
-    </div>
-</div>
-</div>
-`
+    })
+    //.done(function ( data ) {
+    //     userObject.key = data.name;
+    //     key = data.name;
+    // })
+}   
 
+const getDateAndTime = (user) => {
+    user.date = moment().format('MMMM Do YYYY')
+    user.time = moment().format('h:mm:ss a')
+}
+
+ $('#post').click(function() {
+    $("input, textarea").each(function(){
+        let key = this.name
+        let value = this.value
+        userObject[key] = value
+    })
+
+    getDateAndTime(userObject)
+    // userObject.name =  $( 'input:text[name=name]' ).val();
+    // userObject.text =  $( 'textarea[name=content]' ).val();
+    // userObject.url =  $( 'input:text[name=url]' ).val();
+    upload()
+    /*
+    $.ajax({
+        type: 'PATCH',
+        url: `https://taco-database-default-rtdb.firebaseio.com/user/users/crud/${key}/userObject/key.json`,
+        data: userObject.key,
+}); 
+     */
+        /* success and error handling omitted for brevity */
+ })
+
+// const getInfo = () => {
+//     let postCollection;
+//     $.ajax({
+//         method: "GET",
+//         url: "https://crud-ed62d-default-rtdb.firebaseio.com/.json",
+//     })
+// }
+
+/*let gett = function (key) {
+    $.ajax({
+        method: 'GET',
+        url:`https://taco-database-default-rtdb.firebaseio.com/user/users/crud/${key}/userObject/key/.json`
+    })
+}*/
+var get = $.get( `https://taco-database-default-rtdb.firebaseio.com/user/users/crud/-MYN_rClNeZoming-4Rd.json`, function() {
+    console.log( "success" );
+  })
+    .done(function() {
+
+      console.log( "second success" );
+    })
+    .fail(function() {
+      console.log( "error" );
+    })
+    .always(function() {
+      console.log( "finished" );
+    });
+   
+//   // Perform other work here ...
+   
+//   // Set another completion function for the request above
 
 let postBodyHTML = `
                 <div class="row no-gutters">
@@ -78,14 +143,38 @@ let replyHTML = `
                     <ul class="list-group">
                         <li class="list-group-item">
                             <div class="reply-box">
-                                <h3><img src="${profile.src}" alt=""><span>${user}</span></h3>
-                                <p>${comment}</p>
+                                <h3><img src="David" alt=""><span>David</span></h3>
+                                <p>ta cool</p>
                                 <p class="text-right text-muted">
-                                    <span class="date">${date}</span>
-                                    <span class="time">"${time}"</span>
+                                    <span class="date">hoy</span>
+                                    <span class="time">"ahorita"</span>
                                 </p>
                             </div>
                         </li>
                     </ul>
                 </div>
 `
+
+
+let cardHTML = `
+<div class="container">
+<div class="row">
+    <div class="col-12">
+        <div class="card mb-3">
+            ${postBodyHTML}
+            ${replyHTML}
+            <div class="reply-form">
+            <form action="">
+                <div class="form-group d-flex">
+                    <input type="text" class="form-control">
+                    <button class="btn btn-success">></button>
+                </div>
+            </form>
+        </div>
+        </div>
+    </div>
+</div>
+</div>
+`
+
+
