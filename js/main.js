6eff93dcde112757bbdf5817447269bf8c86c701
Risoxas <davidag93@gmail.com>
2021-04-19 11:06:07 -0500

    fixed modal problem

--- js/main.js
+++ js/main.js
@@ -32,33 +32,29 @@
 //     }
 // }
 let userObject = {
-    key: '',
     id: '',
     name: '',
     title: '',
-    text: '',
+    content: '',
     url: '',
     date: '',
-    time: '',
+    time: ''
 };
-let key = '';
+
 let upload = function() {
+    console.log(userObject, typeof userObject)
     $.ajax({
         method: "POST",
-        url: "https://taco-database-default-rtdb.firebaseio.com/user/users/crud.json",
+        url: "https://crud-ed62d-default-rtdb.firebaseio.com/posts/.json",
         data: JSON.stringify({userObject}),
         success : response => {
             console.log(response)
         },
         error : error => {
             console.log(error)
-        },
+        }
 
     })
-    //.done(function ( data ) {
-    //     userObject.key = data.name;
-    //     key = data.name;
-    // })
 }   
 
 const getDateAndTime = (user) => {
@@ -66,115 +62,82 @@ const getDateAndTime = (user) => {
     user.time = moment().format('h:mm:ss a')
 }
 
- $('#post').click(function() {
-    $("input, textarea").each(function(){
+ $('#post').on("click",function() {
+    $("input, textarea, .modal-body").not(".reply").each(function(){
+        console.log(this)
         let key = this.name
         let value = this.value
         userObject[key] = value
+        console.log(userObject)
     })
 
     getDateAndTime(userObject)
-    // userObject.name =  $( 'input:text[name=name]' ).val();
-    // userObject.text =  $( 'textarea[name=content]' ).val();
-    // userObject.url =  $( 'input:text[name=url]' ).val();
+    userObject.id = '1205'
+    userObject.name = "taco"
+    userObject.key = 'post1'
     upload()
-    /*
-    $.ajax({
-        type: 'PATCH',
-        url: `https://taco-database-default-rtdb.firebaseio.com/user/users/crud/${key}/userObject/key.json`,
-        data: userObject.key,
-}); 
-     */
-        /* success and error handling omitted for brevity */
- })
+ });
 
-// const getInfo = () => {
-//     let postCollection;
-//     $.ajax({
-//         method: "GET",
-//         url: "https://crud-ed62d-default-rtdb.firebaseio.com/.json",
-//     })
-// }
-
-/*let gett = function (key) {
-    $.ajax({
-        method: 'GET',
-        url:`https://taco-database-default-rtdb.firebaseio.com/user/users/crud/${key}/userObject/key/.json`
-    })
-}*/
-var get = $.get( `https://taco-database-default-rtdb.firebaseio.com/user/users/crud/-MYN_rClNeZoming-4Rd.json`, function() {
-    console.log( "success" );
-  })
-    .done(function() {
-
-      console.log( "second success" );
-    })
-    .fail(function() {
-      console.log( "error" );
-    })
-    .always(function() {
-      console.log( "finished" );
-    });
    
 //   // Perform other work here ...
    
 //   // Set another completion function for the request above
 
-let postBodyHTML = `
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img class="w-100" src="${key.coverURL}" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${key.title}</h5>
-                            <p class="card-text">${key.content}</p>
-                            <p class="card-text">
-                                <small class="text-muted">Creado el 
-                                    <span class="text-dark">${key.creationDate}</span>
-                                </small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                `
-let replyHTML = `
-                <div class="replies-wrapper">
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <div class="reply-box">
-                                <h3><img src="David" alt=""><span>David</span></h3>
-                                <p>ta cool</p>
-                                <p class="text-right text-muted">
-                                    <span class="date">hoy</span>
-                                    <span class="time">"ahorita"</span>
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-`
+// let postBodyHTML = `
+//                 <div class="row no-gutters">
+//                     <div class="col-md-4">
+//                         <img class="w-100" src="${key.coverURL}" alt="...">
+//                     </div>
+//                     <div class="col-md-8">
+//                         <div class="card-body">
+//                             <h5 class="card-title">${key.title}</h5>
+//                             <p class="card-text">${key.content}</p>
+//                             <p class="card-text">
+//                                 <small class="text-muted">Creado el 
+//                                     <span class="text-dark">${key.creationDate}</span>
+//                                 </small>
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 `
+// let replyHTML = `
+//                 <div class="replies-wrapper">
+//                     <ul class="list-group">
+//                         <li class="list-group-item">
+//                             <div class="reply-box">
+//                                 <h3><img src="David" alt=""><span>David</span></h3>
+//                                 <p>ta cool</p>
+//                                 <p class="text-right text-muted">
+//                                     <span class="date">hoy</span>
+//                                     <span class="time">"ahorita"</span>
+//                                 </p>
+//                             </div>
+//                         </li>
+//                     </ul>
+//                 </div>
+// `
 
 
-let cardHTML = `
-<div class="container">
-<div class="row">
-    <div class="col-12">
-        <div class="card mb-3">
-            ${postBodyHTML}
-            ${replyHTML}
-            <div class="reply-form">
-            <form action="">
-                <div class="form-group d-flex">
-                    <input type="text" class="form-control">
-                    <button class="btn btn-success">></button>
-                </div>
-            </form>
-        </div>
-        </div>
-    </div>
-</div>
-</div>
-`
+// let cardHTML = `
+// <div class="container">
+// <div class="row">
+//     <div class="col-12">
+//         <div class="card mb-3">
+//             ${postBodyHTML}
+//             ${replyHTML}
+//             <div class="reply-form">
+//             <form action="">
+//                 <div class="form-group d-flex">
+//                     <input type="text" class="form-control">
+//                     <button class="btn btn-success">></button>
+//                 </div>
+//             </form>
+//         </div>
+//         </div>
+//     </div>
+// </div>
+// </div>
+// `
 
 
